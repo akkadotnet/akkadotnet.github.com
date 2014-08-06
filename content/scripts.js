d51549909ef21fe6b18015cf441a1511d5ddd251
Roger Alsing <rogeralsing@gmail.com>
2014-08-06 13:25:07 +0200

    Update scripts.js

--- content/scripts.js
+++ content/scripts.js
@@ -1,1 +1,54 @@
-//
+$(function() {
+	$('#toc').toc({
+		'listType': '<ul class="side-nav list-group" />',
+	    'selectors': 'h1,h2,h3', //elements to use as headings
+	    'container': '#main_content', //element to find all selectors in
+	    'headerText': function(i, heading, $heading) { //custom function building the header-item text
+	        return $heading.text();
+	    },
+		'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
+		  return "list-group-item toc-" + $heading[0].tagName.toLowerCase();
+		},
+		activeClass: "active"
+	});
+
+	$.getJSON("http://cdn.syndication.twimg.com/widgets/timelines/495982116597796864?dnt=true&domain=unquietcode.com&lang=en&callback=?", function(data) {
+    var tweets = $(data.body).find('li.tweet');
+    Tweets = [];
+    
+    for (var i=0; i < tweets.length; ++i) {
+      var cur = $(tweets[i]);         
+      var tweet = {};
+      tweet.authorImg = cur.find("img").attr("src");
+      tweet.authorFullName = cur.find("span.full-name span.p-name").html();
+      tweet.authorUserName = cur.find("span.p-nickname b").html();
+      tweet.date = cur.find("a.u-url").attr("data-datetime");
+      tweet.id = cur.attr("data-tweet-id");
+      tweet.text = $.trim(cur.find("p.e-entry-title").html());
+    
+      Tweets.push(tweet);
+      if (i>=3)
+      	break;
+    }
+    console.info(Tweets);
+
+    var tweetsContainer = $('#recent_tweets')
+    $.each(Tweets, function(i)
+    {
+        var li = $('<li/>')
+            .appendTo(tweetsContainer);
+    
+    	$('<i class="fa fa-twitter"/>')
+            .appendTo(li);
+    
+        $('<a/>')
+        	.attr('href', 'https://twitter.com/' + Tweets[i].authorUserName)
+        	.html('<strong> @' + Tweets[i].authorUserName + ": </strong>")
+            .appendTo(li);
+    
+        $('<span/>')
+            .html(Tweets[i].text)
+            .appendTo(li);
+    });
+  });
+});
