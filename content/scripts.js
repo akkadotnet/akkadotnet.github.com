e7fe5ef48adc0620f4bbaf28734687f2df49af30
rogeralsing <rogeralsing@gmail.com>
2014-08-15 10:40:49 +0000

    bla

--- content/scripts.js
+++ content/scripts.js
@@ -57,8 +57,22 @@ $(function() {
 	});
 	
 	var main = $("#main_content");
+	main.find("img").addClass("img-responsive appear-animation fadeInUp");
+	main.find("table").addClass("table table-bordered");
+	var warnings = main.find('blockquote > p > strong')
+	.filter(function(){ return $(this).text().toLowerCase().indexOf('warning') === 0;}).parent().parent();
+	var notes = main.find('blockquote > p > strong')
+	.filter(function(){ return $(this).text().toLowerCase().indexOf('note') === 0;}).parent().parent();
+	
+	warnings.replaceWith(function () {
+    		return $("<div class='alert alert-warning' />").append($(this).contents());
+	});
+	
+	notes.replaceWith(function () {
+    		return $("<div class='alert alert-success' />").append($(this).contents());
+	});
 
-	$.get( "/pages.html", function( data ) {
+	$.get( "/wiki/pages.html", function( data ) {
 		var files = JSON.parse( data ).pages;
 		console.info(files);
 		var codes = main.find("code").filter(function() { 
